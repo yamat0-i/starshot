e655c1bdba2cce9f67f25bed3a261a18767c39d7
yamat0-i <yamatoiida_61@outlook.jp>
2024-11-04 17:03:41 +0900

    Update: Riemann sum

--- acceleration_distance.py
+++ acceleration_distance.py
@@ -2,7 +2,10 @@ import numpy as np
 from scipy import integrate
 
 import calc_gamma as g
+import doppler as d
 
+
+c = 3e8 # Light speed [m/s]
 
 # Sail mass
 def calc_ms(rho, thickness, A):
@@ -14,44 +17,53 @@ rho = 5060 # MoS2 [kg/m^3]
 thickness = 59e-9 # Sail thickness [m]
 A = 10 # Sail area [m^2]
 ms = calc_ms(rho, thickness, A)
-print('ms: ', ms)
 
 mp = 0.1e-3 # Payload mass [kg]
 mt = ms + mp # Total mass [kg]
-c = 3e8 # Light speed [m/s]
 
 P = 10e9 # Laser power [W]
 I = P / A # Laser intensity [W/m^2]
 
-
-
-# R = 0.68 # Reflectance
-fname = 'sweep_1.txt'
+# Load data
+fname = 'sweepver3_7.txt'
 data = np.loadtxt(fname, delimiter=',', skiprows=3)
 
-lam = data[439:671, 0]
-R = data[439:671, 1]
+lam_data = data[:, 0]
+R_data = -data[:, 1]
+
+lam_0 = 1e-6
+lam_f = 1.22 * lam_0
+
+i_0 = np.abs(lam_data-lam_0).argmin() # Index of lam_0
+i_f = np.abs(lam_data-lam_f).argmin() # Index of lam_f
+
+lam = lam_data[i_f:i_0]
+R = R_data[i_f:i_0]
+
+def calc_r_shift(v):
+    lam_shift = d.doppler_shift(lam=lam_0, v=v, c=c)
+    i_shift = np.abs(lam-lam_shift).argmin()
+    R_shift = R[i_shift]
+    return R_shift
 
 # Set velocity
-v_min = 0
+v_0 = 0
 v_f = 0.2 * c
+dv = (v_f - v_0) / len(lam)
 
-v = np.linspace(v_min, v_f, len(lam))
-
-def calc_momentum(mt, reflectance, gamma, v, c):
+def calc_momentum(mt, reflectance, v, c):
     gamma = g.calc_gamma(v, c)
     return (mt / reflectance) * (gamma * v / (1 - v/c)**2)
 
-momentum = []
-
-"""
-
-energy, err = integrate.quad(momentum, v_min, v_f)
+# integrate
+s = 0
+for i in range(1, len(lam)):
+    v1 = v_0 + dv * i
+    reflectance = calc_r_shift(v1)
+    f1 = calc_momentum(mt=mt, reflectance=reflectance, v=v1, c=c)
+    s += dv * f1
 
 coef = c / (2 * I * A) # 1/force
+D = coef * s # Acceleration distance
 
-D = coef * energy # Acceleration distance
-
-print('D:', D)
-print('error:', err)
-"""
+print('D[m]: {:e}'.format(D))
--- doppler.py
+++ doppler.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 import calc_gamma as g
 
 c = 3e8
 
-def doppler_shift(nu, v, c):
-    nu_shift =  nu / (g.calc_gamma(v, c) * (1 - v/c))
-    return nu_shift
+def doppler_shift(lam, v, c):
+    nu = lam2nu(lam)
+    gamma = g.calc_gamma(v, c)
+    nu_shift =  nu / (np.sqrt(gamma) * (1 + v/c))
+    lam_shift = nu2lam(nu_shift)
+    return lam_shift
 
 def lam2nu(lam):
     nu = c / lam
@@ -14,3 +19,8 @@ def nu2lam(nu):
     lam = c / nu
     return lam
 
+if __name__ == '__main__':
+    v = np.linspace(0, 0.2*c, 50)
+    lam = 1e-6
+    lam_shift = doppler_shift(lam=lam, v=v, c=c)
+    print(lam_shift)
